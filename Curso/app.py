8a0989d413c1a8ac8293fab5132281f4369e0723
Robson Luiz <robson.luizsilva2019@gmail.com>
2024-11-07 22:16:29 -0300

    Inicio 1 aula

---
+++ Curso/app.py
@@ -0,0 +1,34 @@
+ # Tipos de Dados
+
+print(-975)         # prints: -975
+print("Infinito")   # prints: Infinito
+print('Simon')      # prints: Simon
+print('positivo @') # prints: positivo @
+#===========================================================
+
+#Python usa "[]" para acessar os itens de uma sequencia
+
+print("Hard Times" [5]) # prints: T
+print("Girafa" [0])     # prints: G
+#===========================================================
+
+#Converter um item de dados de um tipo para outro
+#Sintaxe  --datetype(item)--
+
+print(int("45")) # prints: 45 
+print(str(975))  # prints: 975
+#===========================================================
+
+#Referencia de objeto
+#Sintaxe  --referencia de objeto(Indicadores) = valor
+
+x = "Blue"
+y = "Green"
+z = x
+print(x, y, z) # prints: Blue Green Blue
+z = y
+print(x, y, z) # prints: Blue Green Green
+x = z
+print(x, y, z) # prints: Green Green Green
+#=============================================================
+
