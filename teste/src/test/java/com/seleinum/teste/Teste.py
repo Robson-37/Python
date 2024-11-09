cc3552514d3aece00cd5598c0e7c8ee53d64b3e8
Robson Luiz <robson.luizsilva2019@gmail.com>
2024-11-09 00:58:24 -0300

    Atualizado

--- teste/src/test/java/com/seleinum/teste/Teste.py
+++ teste/src/test/java/com/seleinum/teste/Teste.py
@@ -1,2 +1,34 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+import time
+import pyautogui
+import pyscreeze
+
+class TesteSelenium(unittest.TestCase):
+
+    def setUp(self):
+        # Inicialização do driver chrome
+        options = webdriver.ChromeOptions()
+
+        # Maximiza a janela do navegador
+        options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(options=options)
+
+    def test_google(self):
+        self.driver.get("https://www.google.com.br")
+        time.sleep(5)
+        self.tirar_print()
+
+    def tirar_print(self):
+       self.imagem = pyautogui.screenshot()
+       self.imagem.save('C:/Users/robso/OneDrive/Documentos/CursoPython/Python/teste/img/print.png')
+   
+
+    def tearDown(self):
+        self.driver.quit()
+        
+if __name__=='__main__':
+ unittest.main()
